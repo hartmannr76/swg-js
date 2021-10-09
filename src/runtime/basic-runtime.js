929b2fe252341d6305d4ca92c8208797d9ed7779
mmmicedcoffee <irenechen@google.com>
2021-10-08 18:18:05 -0700

    Add forceLangInIframes option to ClientOptions.

--- src/runtime/basic-runtime.js
+++ src/runtime/basic-runtime.js
@@ -196,7 +196,9 @@ export class BasicRuntime {
         this.configured_(true);
       });
 
-    this.clientOptions_ = params.clientOptions;
+    this.clientOptions_ = Object.assign({}, params.clientOptions, {
+      forceLangInIframes: true,
+    });
     this.setupAndShowAutoPrompt({
       autoPromptType: params.autoPromptType,
       alwaysShow: params.alwaysShow || false,
