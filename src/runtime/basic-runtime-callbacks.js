507447042aac87b9b0503a620e3d94da609baedd
Ricky Hartmann <hartmannr76@gmail.com>
2022-04-07 13:29:11 +0000

    chore: Move the documentation over

--- src/runtime/basic-runtime-callbacks.js
+++ src/runtime/basic-runtime-callbacks.js
@@ -5,7 +5,11 @@ export class BasicRuntimeCallbacks extends Callbacks {
     super();
   }
 
-  /** @override */
+  /**
+   * Set the default entitlements response handler to consume a valid metering entitlement
+   * while still allowing the user to have their own handler.
+   * @override
+   * */
   triggerEntitlementsResponse(promise) {
     promise.then((entitlementResults) => {
       const entitlements = entitlementResults.clone();
